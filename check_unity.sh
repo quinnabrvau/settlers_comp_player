f4ecf77474f31b6719b95d5b0d774b944e1a86db
Quinn Abrahams-Vaughn <quinna@wearstrive.com>
2019-01-10 08:27:54 -0800

    added code to check that the unity sub system was downloaded

---
+++ check_unity.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -d 'unity' ]; then
+  echo 'unity directory exists with:'
+  ls 'unity'
+  cd 'unity'
+  if [ -d 'src' ]; then
+    echo 'src directory exists with:'
+    ls 'src'
+  fi
+  cd ..
+fi
+
+if [ ! -d 'unity' ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  ls
+  git 'clone' 'https://github.com/ThrowTheSwitch/Unity.git' 'unity'
+  sh ./check_unity.sh
+fi
+exit 0
