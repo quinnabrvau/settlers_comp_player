a9de54f28d00c12657adde6d4085807adefed885
Quinn Abrahams-Vaughn <quinna@wearstrive.com>
2019-01-10 08:32:48 -0800

    foo

--- check_unity.sh
+++ check_unity.sh
@@ -4,6 +4,9 @@ if [ -d 'unity' ]; then
   echo 'unity directory exists with:'
   ls 'unity'
   cd 'unity'
+  if [ ! -d 'src' ]; then
+    git 'clone' 'https://github.com/ThrowTheSwitch/Unity.git' '.'
+  fi
   if [ -d 'src' ]; then
     echo 'src directory exists with:'
     ls 'src'
