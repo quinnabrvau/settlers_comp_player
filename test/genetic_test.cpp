cdfb394f5100ac3890c7558d7b7c685ed27ce3e1
Quinn Abrahams-Vaughn <quinna@wearstrive.com>
2019-01-11 07:52:09 -0800

    added genetic test program

---
+++ test/genetic_test.cpp
@@ -0,0 +1,17 @@
+//
+//  genetic_test.cpp
+//  GAME_TEST_LIB
+//
+//  Created by Quinn Abrahams-Vaughn on 1/11/19.
+//
+
+#include "Array.hpp"
+#include "Genetic.hpp"
+
+#include "unity.h"
+
+int main(int argc, const char * argv[]) {
+    UNITY_BEGIN();
+    test__Array();
+    return UNITY_END();
+}
